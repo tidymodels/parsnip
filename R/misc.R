79ef34d103aaffaf54fc78d22c3b3dba5326e70f
Max Kuhn <mxkuhn@gmail.com>
2018-01-24 15:18:30 -0500

    function to check for empty ...

--- R/misc.R
+++ R/misc.R
@@ -19,7 +19,7 @@ make_classes <- function(prefix, mode) {
 
 
 get_model_objects <- function(x, engine) {
-  if(mode == "unknown")
+  if(x$mode == "unknown")
     stop("Please specify a mode for the model (e.g. regression, classification, etc.) ", 
          "so that the model code can be finalized", call. = FALSE)
   nm <- paste("get", engine, x$mode, sep = "_")
@@ -27,5 +27,13 @@ get_model_objects <- function(x, engine) {
   if(inherits(res, "try-error"))
     stop("Can't find model object ", nm)
   res
+}
+
+#' @importFrom rlang quos is_empty is_null
+check_empty_ellipse <- function (...)  {
+  terms <- quos(...)
+  if (!is_empty(terms)) 
+    stop("Please pass other arguments to the model function via `engine_args`", call. = FALSE)
+  terms
 }
 
