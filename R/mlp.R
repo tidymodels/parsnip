beb869f3a49e6af89de4c93af8988b21e340f615
topepo <mxkuhn@gmail.com>
2018-06-09 20:20:21 -0400

    added simple neural networks

---
+++ R/mlp.R
@@ -0,0 +1,200 @@
+#' General Interface for Single Layer Neural Network
+#'
+#' `mlp`, for multilayer perceptron, is a way to generate a _specification_ of
+#'  a model before fitting and allows the model to be created using
+#'  different packages in R or via keras The main arguments for the
+#'  model are:
+#' \itemize{
+#'   \item \code{units}: The number of units in the hidden layer.
+#'   \item \code{weight_decay}: The amount of L2 regularization (aka weight
+#'     decay).
+#'   \item \code{dropout}: The proportion of parameters randomly dropped out of
+#'     the model (`keras` only).
+#'   \item \code{epochs}: The number of training iterations.
+#'   \item \code{activation}: The type of function that connects the hidden
+#'     layer and the input variables  (`keras` only).
+#' }
+#' These arguments are converted to their specific names at the
+#'  time that the model is fit. Other options and argument can be
+#'  set using the `others` argument. If left to their defaults
+#'  here (`NULL`), the values are taken from the underlying model
+#'  functions. One exception is `units` when `nnet::nnet` is used; that
+#'  function's `size` argument has no default so a value of 5 units will be
+#'  used. Also, unless otherwise specified, the `linout` argument to
+#'  `nnet::nnet` will be set to `TRUE` when a regression model is created.
+#'
+#' The data given to the function are not saved and are only used
+#'  to determine the _mode_ of the model. For `mlp`, the
+#'  possible modes are "regression" and "classification".
+#'
+#' The model can be created using the [fit()] function using the
+#'  following _engines_:
+#' \itemize{
+#' \item \pkg{R}:  `"nnet"`
+#' \item \pkg{keras}: `"keras"`
+#' }
+#' @param mode A single character string for the type of model.
+#'  Possible values for this model are "unknown", "regression", or
+#'  "classification".
+#' @param others A named list of arguments to be used by the
+#'  underlying models (e.g., `nnet::nnet`,
+#'  `keras::fit`, `keras::compile`, etc.). .
+#' @param units An integer for the number of units in the hidden model.
+#' @param weight_decay A non-negative numeric value for the amount of weight
+#'  decay.
+#' @param dropout A number between 0 (inclusive) and 1 denoting the proportion
+#'  of model parameters randomly set to zero during model training.
+#' @param epochs An integer for the number of training iterations.
+#' @param activation A single character strong denoting the type of relationship
+#'  between the original predictors and the hidden unit layer. The activation
+#'  function between the hidden and output layers is automatically set to either
+#'  "linear" or "softmax" depending on the type of outcome. Possible values are:
+#'  "linear", "softmax", "relu", and "elu"
+#' @param ... Used for method consistency. Any arguments passed to
+#'  the ellipses will result in an error. Use `others` instead.
+#' @details Main parameter arguments (and those in `others`) can avoid
+#'  evaluation until the underlying function is executed by wrapping the
+#'  argument in [rlang::expr()] (e.g. `units = expr(num_preds * 2)`).
+#'
+#'  An error is thrown if both `weight_decay` and `dropout` are specified for
+#'  `keras` models.
+#' @importFrom purrr map_lgl
+#' @seealso [varying()], [fit()]
+#' @examples
+#' mlp(mode = "classification", weight_decay = 0.01)
+#' # Parameters can be represented by a placeholder:
+#' mlp(mode = "regression", units = varying())
+#' @export
+
+mlp <-
+  function(mode = "unknown",
+           units = NULL, weight_decay = NULL, dropout = NULL, epochs = NULL,
+           activation = NULL,
+           others = list(),
+           ...) {
+    check_empty_ellipse(...)
+
+    act_funs <- c("linear", "softmax", "relu", "elu")
+    if (is.numeric(units))
+      if (units < 2)
+        stop("There must be at least two hidden units", call. = FALSE)
+    if (is.numeric(weight_decay))
+      if (weight_decay < 0)
+        stop("The amount of weight decay must be >= 0.", call. = FALSE)
+    if (is.numeric(dropout))
+      if (dropout < 0 | dropout >= 1)
+        stop("The dropout proportion must be on [0, 1).", call. = FALSE)
+    if (is.numeric(weight_decay) & is.numeric(dropout))
+      if (dropout > 0 & weight_decay > 0)
+        stop("Both weight decay and dropout should not be specified.", call. = FALSE)
+    if (is.character(activation))
+      if (!any(activation %in% c(act_funs)))
+        stop("`activation should be one of: ",
+             paste0("'", act_funs, "'", collapse = ", "),
+             call. = FALSE)
+
+    if (!(mode %in% mlp_modes))
+      stop("`mode` should be one of: ",
+           paste0("'", mlp_modes, "'", collapse = ", "),
+           call. = FALSE)
+
+    args <- list(units = units, weight_decay = weight_decay, dropout = dropout,
+                 epochs = epochs, activation = activation)
+
+    no_value <- !vapply(others, is.null, logical(1))
+    others <- others[no_value]
+
+    # write a constructor function
+    out <- list(args = args, others = others,
+                mode = mode, method = NULL, engine = NULL)
+    # TODO: make_classes has wrong order; go from specific to general
+    class(out) <- make_classes("mlp")
+    out
+  }
+
+#' @export
+print.mlp <- function(x, ...) {
+  cat("Single Layer Neural Network Specification (", x$mode, ")\n\n", sep = "")
+  model_printer(x, ...)
+
+  if(!is.null(x$method$fit_args)) {
+    cat("Model fit template:\n")
+    print(show_call(x))
+  }
+  invisible(x)
+}
+
+###################################################################
+
+#' Update a Single Layer Neural Network Specification
+#'
+#' If parameters need to be modified, this function can be used
+#'  in lieu of recreating the object from scratch.
+#'
+#' @export
+#' @inheritParams mlp
+#' @param object A random forest model specification.
+#' @param fresh A logical for whether the arguments should be
+#'  modified in-place of or replaced wholesale.
+#' @return An updated model specification.
+#' @examples
+#' model <- mlp(units = 10, dropout = 0.30)
+#' model
+#' update(model, units = 2)
+#' update(model, units = 2, fresh = TRUE)
+#' @method update mlp
+#' @rdname mlp
+#' @export
+update.mlp <-
+  function(object,
+           units = NULL, weight_decay = NULL, dropout = NULL,
+           epochs = NULL, activation = NULL,
+           others = list(),
+           fresh = FALSE,
+           ...) {
+    check_empty_ellipse(...)
+
+    args <- list(units = units, weight_decay = weight_decay, dropout = dropout,
+                 epochs = epochs, activation = activation)
+
+    # TODO make these blocks into a function and document well
+    if (fresh) {
+      object$args <- args
+    } else {
+      null_args <- map_lgl(args, null_value)
+      if (any(null_args))
+        args <- args[!null_args]
+      if (length(args) > 0)
+        object$args[names(args)] <- args
+    }
+
+    if (length(others) > 0) {
+      if (fresh)
+        object$others <- others
+      else
+        object$others[names(others)] <- others
+    }
+
+    object
+  }
+
+###################################################################
+
+#' @export
+translate.mlp <- function(x, engine, ...) {
+
+  if (engine == "nnet") {
+    if(is.null(x$args$units))
+      x$args$units <- 5
+  }
+
+  x <- translate.default(x, engine, ...)
+
+  if (engine == "nnet") {
+    if (x$mode == "classification") {
+      if (length(x$others) == 0  || !any(names(x$others) == "linout"))
+        x$method$fit_args$linout <- FALSE
+    }
+  }
+  x
+}
