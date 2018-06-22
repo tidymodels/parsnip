2a2ddeea34ddcf564b0fddfd5bc56e9ac34cd969
topepo <mxkuhn@gmail.com>
2018-06-21 22:20:44 -0400

    linting and added proper `newdata` value

--- R/predict.R
+++ R/predict.R
@@ -1,46 +1,50 @@
 #' Regression Model Predictions
-#' 
+#'
 #' Apply a model to create predictions when the outcome is a simple
-#' number. 
-#' 
+#' number.
+#'
 #' @param object An object of class `model_fit`
 #' @param newdata A rectangular data object, such as a data frame.
 #' @param ... Arguments to pass to other methods (not currently used).
-#' @return An unnamed numeric vector of predictions. 
+#' @return An unnamed numeric vector of predictions.
 #' @method predict model_fit
 #' @export predict.model_fit
 #' @export
 predict.model_fit <- function (object, newdata, ...) {
-  if(object$spec$mode != "regression")
+  if (object$spec$mode != "regression")
     stop("`predict.model_fit` is for predicting numeric outcomes.  ",
          "Use `predict_class` or `predict_prob` for ",
          "classification models.", call. = FALSE)
   fit_interface <- object$spec$method$fit$interface
-  
-  if(!is.na(object$preproc)) {
+
+  if (!all(is.na(object$preproc))) {
     # Translation code
-    if(fit_interface == "formula") {
+    if (fit_interface == "formula") {
       newdata <- parsnip:::convert_xy_to_form_new(object$preproc, newdata)
     } else {
-      newdata <- parsnip:::convert_xy_to_form_new(object$preproc, newdata)
+      newdata <- parsnip:::convert_form_to_xy_new(object$preproc, newdata)$x
     }
   }
-  
+
   # preprocess data
-  if(!is.null(object$spec$method$pred$pre)) {
+  if (!is.null(object$spec$method$pred$pre)) {
     newdata <- object$spec$method$pred$pre(newdata, object)
   }
-  
+
   # create prediction call
   pred_call <- make_pred_call(object$spec$method$pred)
-  
+
   res <- eval_tidy(pred_call)
   # post-process the predictions
-  
-  if(!is.null(object$spec$method$pred$post)) {
+
+  if (!is.null(object$spec$method$pred$post)) {
     res <- object$spec$method$pred$post(res, object)
   }
-  
+
+  if (is.vector(res)) {
+    res <- unname(res)
+  } else
+    res <- as.data.frame(res)
   res
 }
 
@@ -51,7 +55,7 @@ make_pred_call <- function(x) {
       call2(x$func["fun"],!!!x$args, .ns = x$func["pkg"])
   else
     cl <-   call2(x$func["fun"],!!!x$args)
-  
+
   cl
 }
 
