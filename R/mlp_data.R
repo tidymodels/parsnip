eadb71cc59a97f91317fbe3003ad3927ee189a84
topepo <mxkuhn@gmail.com>
2018-08-11 16:30:52 -0400

    bug fix for univariate nnet predictions

--- R/mlp_data.R
+++ R/mlp_data.R
@@ -20,7 +20,7 @@ maybe_multivariate <- function(results, object) {
   if (isTRUE(ncol(results) > 1))
     results <- as_tibble(results)
   else
-    unname(results[, 1])
+    results <- unname(results[, 1])
   results
 }
 
