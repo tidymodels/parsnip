5603c9b35356339081f0723242c7705ea198d146
topepo <mxkuhn@gmail.com>
2018-08-11 17:47:55 -0400

    first pass at issues #34 and #41

--- NEWS.md
+++ NEWS.md
@@ -2,3 +2,4 @@
 
 * The `fit` interface was previously used to cover both the x/y interface as well as the formula interface. Now, `fit` is the formula interface and [`fit_xy` is for the x/y interface](https://github.com/topepo/parsnip/issues/33). 
 * Added a `NEWS.md` file to track changes to the package.
+* `predict` methods were [overhauled](https://github.com/topepo/parsnip/issues/34) to be [consistent](https://github.com/topepo/parsnip/issues/41).
---
+++ R/predict_num.R
@@ -0,0 +1,43 @@
+#' @rdname predict.model_fit
+#' @inheritParams predict.model_fit
+#' @method predict_num model_fit
+#' @export predict_num.model_fit
+# TODO add ...
+predict_num.model_fit <- function (object, newdata, ...) {
+  if (object$spec$mode != "regression")
+    stop("`predict_num` is for predicting numeric outcomes.  ",
+         "Use `predict_class` or `predict_prob` for ",
+         "classification models.", call. = FALSE)
+  
+  if (!any(names(object$spec$method) == "pred"))
+    stop("No prediction module defined for this model.", call. = FALSE)
+  
+  newdata <- prepare_data(object, newdata)
+  
+  # preprocess data
+  if (!is.null(object$spec$method$pred$pre))
+    newdata <- object$spec$method$pred$pre(newdata, object)
+  
+  # create prediction call
+  pred_call <- make_pred_call(object$spec$method$pred)
+  
+  res <- eval_tidy(pred_call)
+  # post-process the predictions
+  
+  if (!is.null(object$spec$method$pred$post)) {
+    res <- object$spec$method$pred$post(res, object)
+  }
+  
+  if (is.vector(res)) {
+    res <- unname(res)
+  } else
+    res <- as.data.frame(res)
+  res
+}
+
+
+#' @export
+#' @rdname predict.model_fit
+#' @inheritParams predict_num.model_fit
+predict_num <- function (object, ...)
+  UseMethod("predict_num")
