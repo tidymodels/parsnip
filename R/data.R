999fb973e94e604e769150ec2953ac1c767324fb
topepo <mxkuhn@gmail.com>
2018-09-01 15:47:44 -0400

    added another data set (for classification and survival analysis)

--- R/data.R
+++ R/data.R
@@ -20,3 +20,27 @@
 #' data(lending_club)
 #' str(lending_club)
 NULL
+
+
+#' Watson Churn Data
+#'
+#' @details These data were downloaded from the IBM Watson site
+#'  (see below) in September 2018. The data contain a factor for
+#'  whether a customer churned or not. Alternatively, the `tenure`
+#'  column presumably contains information on how long the customer
+#'  has had an account. A survival analysis can be done on this
+#'  column using the `churn` outcome as the censoring information. A
+#'  data dictionary can be found on the source website.
+#'
+#' @name wa_churn
+#' @aliases wa_churn
+#' @docType data
+#' @return \item{wa_churn}{a data frame}
+#'
+#' @source IBM Watson Analytics https://ibm.co/2sOvyvy
+#'
+#' @keywords datasets
+#' @examples
+#' data(wa_churn)
+#' str(wa_churn)
+NULL
