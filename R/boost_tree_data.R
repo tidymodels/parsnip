8d735c90ad46d1a2d8d0706857db29c254c12974
topepo <mxkuhn@gmail.com>
2018-09-21 16:30:32 -0400

    C5.0 submodel predictions

--- R/boost_tree_data.R
+++ R/boost_tree_data.R
@@ -133,8 +133,6 @@ multi_predict._xgb.Booster <-
   }
 
 xgb_by_tree <- function(tree, object, new_data, type, ...) {
-  dots <- list(...)
-  dots$ntreelimit <- tree
   pred <- parsnip:::xgb_pred(object$fit, newdata = new_data, ntreelimit = tree)
 
   # switch based on prediction type
@@ -183,6 +181,41 @@ C5.0_train <-
   for(i in names(fit_args))
     fit_call[[i]] <- fit_args[[i]]
   eval_tidy(fit_call)
+}
+
+#' @export
+multi_predict._C5.0 <-
+  function(object, new_data, type = NULL, trees = NULL, ...) {
+    if (is.null(trees))
+      trees <- min(object$fit$trials)
+    trees <- sort(trees)
+
+    if (is.null(type))
+      type <- "class"
+
+    res <-
+      map_df(trees, C50_by_tree, object = object,
+             new_data = new_data, type = type, ...)
+    res <- arrange(res, .row, trees)
+    res <- split(res[, -1], res$.row)
+    names(res) <- NULL
+    tibble(.pred = res)
+  }
+
+C50_by_tree <- function(tree, object, new_data, type, ...) {
+  pred <- predict(object$fit, newdata = new_data, trials = tree, type = type)
+
+  # switch based on prediction type
+  if (type == "class") {
+    pred <- tibble(.pred = factor(pred, levels = object$lvl))
+  } else {
+    pred <- as_tibble(pred)
+    names(pred) <- paste0(".pred_", names(pred))
+  }
+  nms <- names(pred)
+  pred[["trees"]] <- tree
+  pred[[".row"]] <- 1:nrow(new_data)
+  pred[, c(".row", "trees", nms)]
 }
 
 # ------------------------------------------------------------------------------
