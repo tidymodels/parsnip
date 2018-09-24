8035970c2435574d56fb34c5cee1f5c1349ceff0
topepo <mxkuhn@gmail.com>
2018-09-24 14:24:38 -0400

    boosting via spark

--- R/boost_tree_data.R
+++ R/boost_tree_data.R
@@ -1,7 +1,8 @@
 
 boost_tree_arg_key <- data.frame(
-  xgboost = c("max_depth", "nrounds", "eta", "colsample_bytree", "min_child_weight", "gamma", "subsample"),
-  C5.0 =    c(         NA,  "trials",    NA,                NA,         "minCases",       NA,    "sample"),
+  xgboost = c("max_depth",  "nrounds",       "eta",       "colsample_bytree",        "min_child_weight",         "gamma",        "subsample"),
+  C5.0 =    c(         NA,   "trials",          NA,                       NA,                "minCases",              NA,           "sample"),
+  spark =   c("max_depth", "max_iter", "step_size", "feature_subset_strategy", "min_instances_per_node", "min_info_gain", "subsampling_rate"),
   stringsAsFactors = FALSE,
   row.names =  c("tree_depth", "trees", "learn_rate", "mtry", "min_n", "loss_reduction", "sample_size")
 )
@@ -11,6 +12,7 @@ boost_tree_modes <- c("classification", "regression", "unknown")
 boost_tree_engines <- data.frame(
   xgboost =    rep(TRUE, 3),
   C5.0    =    c(            TRUE,        FALSE,      TRUE),
+  spark   =    rep(TRUE, 3),
   row.names =  c("classification", "regression", "unknown")
 )
 
@@ -127,3 +129,47 @@ boost_tree_C5.0_data <-
     )
   )
 
+
+boost_tree_spark_data <-
+  list(
+    libs = "sparklyr",
+    fit = list(
+      interface = "formula",
+      protect = c("x", "formula", "type"),
+      func = c(pkg = "sparklyr", fun = "ml_gradient_boosted_trees"),
+      defaults =
+        list(
+          seed = expr(sample.int(10^5, 1))
+        )
+    ),
+    pred = list(
+      pre = NULL,
+      post = format_spark_num,
+      func = c(pkg = "sparklyr", fun = "ml_predict"),
+      args =
+        list(
+          x = quote(object$fit),
+          dataset = quote(new_data)
+        )
+    ),
+    classes = list(
+      pre = NULL,
+      post = format_spark_class,
+      func = c(pkg = "sparklyr", fun = "ml_predict"),
+      args =
+        list(
+          x = quote(object$fit),
+          dataset = quote(new_data)
+        )
+    ),
+    prob = list(
+      pre = NULL,
+      post = format_spark_probs,
+      func = c(pkg = "sparklyr", fun = "ml_predict"),
+      args =
+        list(
+          x = quote(object$fit),
+          dataset = quote(new_data)
+        )
+    )
+  )
---
+++ tests/testthat/test_boost_tree_spark.R
@@ -0,0 +1,196 @@
+library(testthat)
+context("boosted tree execution with spark")
+library(parsnip)
+library(dplyr)
+
+# ------------------------------------------------------------------------------
+
+ctrl <- fit_control(verbosity = 1, catch = FALSE)
+caught_ctrl <- fit_control(verbosity = 1, catch = TRUE)
+quiet_ctrl <- fit_control(verbosity = 0, catch = TRUE)
+
+# ------------------------------------------------------------------------------
+
+test_that('spark execution', {
+  
+  skip_if_not_installed("sparklyr")
+  
+  library(sparklyr)
+  
+  sc <- try(spark_connect(master = "local"), silent = TRUE)
+  
+  skip_if(inherits(sc, "try-error"))
+  
+  iris_bt_tr <- copy_to(sc, iris[-(1:4),   ], "iris_bt_tr", overwrite = TRUE)
+  iris_bt_te <- copy_to(sc, iris[  1:4 , -1], "iris_bt_te", overwrite = TRUE)
+  
+  # ----------------------------------------------------------------------------
+  
+  expect_error(
+    spark_reg_fit <-
+      fit(
+        boost_tree(
+          mode = "regression",
+          others = list(seed = 12)
+        ),
+        engine = "spark",
+        control = ctrl,
+        Sepal_Length ~ .,
+        data = iris_bt_tr
+      ),
+    regexp = NA
+  )
+  
+  # check for reproducibility and passing extra arguments
+  expect_error(
+    spark_reg_fit_dup <-
+      fit(
+        boost_tree(
+          mode = "regression",
+          others = list(seed = 12)
+        ),
+        engine = "spark",
+        control = ctrl,
+        Sepal_Length ~ .,
+        data = iris_bt_tr
+      ),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_reg_pred <- predict(spark_reg_fit, iris_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_reg_pred_num <- predict_num(spark_reg_fit, iris_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_reg_dup <- predict(spark_reg_fit_dup, iris_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_reg_num_dup <- predict_num(spark_reg_fit_dup, iris_bt_te),
+    regexp = NA
+  )
+  
+  expect_equal(colnames(spark_reg_pred), "pred")
+  
+  expect_equal(
+    as.data.frame(spark_reg_pred)$pred,
+    as.data.frame(spark_reg_dup)$pred
+  )
+  expect_equal(
+    as.data.frame(spark_reg_pred_num)$pred,
+    as.data.frame(spark_reg_num_dup)$pred
+  )
+  
+  
+  # ----------------------------------------------------------------------------
+  
+  # same for classification
+  
+  churn_bt_tr <- copy_to(sc, wa_churn[ 5:100,   ], "churn_bt_tr", overwrite = TRUE)
+  churn_bt_te <- copy_to(sc, wa_churn[   1:4, -1], "churn_bt_te", overwrite = TRUE)
+  
+  # ----------------------------------------------------------------------------
+  
+  expect_error(
+    spark_class_fit <-
+      fit(
+        boost_tree(
+          mode = "classification",
+          others = list(seed = 12)
+        ),
+        engine = "spark",
+        control = ctrl,
+        churn ~ .,
+        data = churn_bt_tr
+      ),
+    regexp = NA
+  )
+  
+  # check for reproducibility and passing extra arguments
+  expect_error(
+    spark_class_fit_dup <-
+      fit(
+        boost_tree(
+          mode = "classification",
+          others = list(seed = 12)
+        ),
+        engine = "spark",
+        control = ctrl,
+        churn ~ .,
+        data = churn_bt_tr
+      ),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_pred <- predict(spark_class_fit, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_pred_class <- predict_class(spark_class_fit, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_dup <- predict(spark_class_fit_dup, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_dup_class <- predict_class(spark_class_fit_dup, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_equal(colnames(spark_class_pred), "pred_class")
+  
+  expect_equal(
+    as.data.frame(spark_class_pred)$pred_class,
+    as.data.frame(spark_class_dup)$pred_class
+  )
+  expect_equal(
+    as.data.frame(spark_class_pred_class)$pred_class,
+    as.data.frame(spark_class_dup_class)$pred_class
+  )
+  
+  
+  expect_error(
+    spark_class_prob <- predict(spark_class_fit, churn_bt_te, type = "prob"),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_prob_classprob <- predict_classprob(spark_class_fit, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_dup <- predict(spark_class_fit_dup, churn_bt_te, type = "prob"),
+    regexp = NA
+  )
+  
+  expect_error(
+    spark_class_dup_classprob <- predict_classprob(spark_class_fit_dup, churn_bt_te),
+    regexp = NA
+  )
+  
+  expect_equal(colnames(spark_class_prob), c("pred_No", "pred_Yes"))
+  
+  expect_equivalent(
+    as.data.frame(spark_class_prob),
+    as.data.frame(spark_class_dup)
+  )
+  expect_equal(
+    as.data.frame(spark_class_prob_classprob),
+    as.data.frame(spark_class_dup_classprob)
+  )
+  
+})
+
