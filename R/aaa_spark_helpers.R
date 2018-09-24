d8573cc073813728d9a8fae5a902046cca2d27af
topepo <mxkuhn@gmail.com>
2018-09-24 13:43:16 -0400

    spark helpers get their own files

---
+++ R/aaa_spark_helpers.R
@@ -0,0 +1,25 @@
+# some spark helper functions
+
+#' @importFrom dplyr starts_with rename rename_at vars funs
+format_spark_probs <- function(results, object) {
+  results <- dplyr::select(results, starts_with("probability_"))
+  results <- dplyr::rename_at(
+    results,
+    vars(starts_with("probability_")),
+    funs(gsub("probability", "pred", .))
+  )
+  results
+}
+
+format_spark_class <- function(results, object) {
+  results <- dplyr::select(results, predicted_label)
+  results <- dplyr::rename(results, pred_class = predicted_label)
+  results
+}
+
+format_spark_num <- function(results, object) {
+  results <- dplyr::select(results, prediction)
+  results <- dplyr::rename(results, pred = prediction)
+  results
+}
+
--- R/rand_forest.R
+++ R/rand_forest.R
@@ -205,31 +205,3 @@ translate.rand_forest <- function(x, engine, ...) {
   x
 }
 
-# ------------------------------------------------------------------------------
-
-# some spark helper functions
-
-#' @importFrom dplyr starts_with rename rename_at vars funs
-format_spark_probs <- function(results, object) {
-  results <- dplyr::select(results, starts_with("probability_"))
-  results <- dplyr::rename_at(
-    results,
-    vars(starts_with("probability_")),
-    funs(gsub("probability", "pred", .))
-    )
-  results
-}
-
-format_spark_class <- function(results, object) {
-  results <- dplyr::select(results, predicted_label)
-  results <- dplyr::rename(results, pred_class = predicted_label)
-  results
-}
-
-format_spark_num <- function(results, object) {
-  results <- dplyr::select(results, prediction)
-  results <- dplyr::rename(results, pred = prediction)
-  results
-}
-
-
