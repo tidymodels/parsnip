0ab34901896294c6716f2dc605248d01d2c31920
topepo <mxkuhn@gmail.com>
2018-09-24 12:07:49 -0400

    documentation improvements

--- R/fit_helpers.R
+++ R/fit_helpers.R
@@ -1,4 +1,7 @@
-# Homogenous interfaces first
+# These functions are the go-betweens between parsnip::fit (or parsnip::fit_xy)
+# and the underlying model function (such as ranger::ranger). So if `fit_xy` is
+# used to fit a ranger model, there needs to be a conversion from x/y format
+# data to formula/data objects and so on.
 
 #' @importFrom  stats model.frame model.response terms as.formula model.matrix
 form_form <-
