3d4ed130fb83af25f280cdf997f4a67f3c1c96d1
alex hayes <alexpghayes@gmail.com>
2018-09-26 11:36:52 -0500

    Remove linear regression prediction logic

--- R/linear_reg_data.R
+++ R/linear_reg_data.R
@@ -29,50 +29,6 @@ linear_reg_lm_data <-
       protect = c("formula", "data", "weights"),
       func = c(pkg = "stats", fun = "lm"),
       defaults = list()
-    ),
-    pred = list(
-      pre = NULL,
-      post = NULL,
-      func = c(fun = "safe_predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          new_data = quote(new_data),
-          type = "response"
-        )
-    ),
-    confint = list(
-      pre = NULL,
-      post = NULL,
-      func = c(fun = "safe_predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          new_data = quote(new_data),
-          type = "conf_int",
-          level = quote(level)
-        )
-    ),
-    predint = list(
-      pre = NULL,
-      post = NULL,
-      func = c(fun = "safe_predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          new_data = quote(new_data),
-          type = "pred_int",
-          level = quote(level)
-        )
-    ),
-    raw = list(
-      pre = NULL,
-      func = c(fun = "predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newdata = quote(new_data)
-        )
     )
   )
 
@@ -86,27 +42,6 @@ linear_reg_glmnet_data <-
       defaults =
         list(
           family = "gaussian"
-        )
-    ),
-    pred = list(
-      pre = NULL,
-      post = organize_glmnet_pred,
-      func = c(fun = "predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newx = quote(as.matrix(new_data)),
-          type = "response",
-          s = quote(object$spec$args$penalty)
-        )
-    ),
-    raw = list(
-      pre = NULL,
-      func = c(fun = "predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newx = quote(as.matrix(new_data))
         )
     )
   )
@@ -122,88 +57,9 @@ linear_reg_stan_data <-
         list(
           family = "gaussian"
         )
-    ),
-    pred = list(
-      pre = NULL,
-      post = NULL,
-      func = c(fun = "predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newdata = quote(new_data)
-        )
-    ),
-    confint = list(
-      pre = NULL,
-      post = function(results, object) {
-        res <-
-          tibble(
-            .pred_lower =
-              convert_stan_interval(
-                results,
-                level = object$spec$method$confint$extras$level
-              ),
-            .pred_upper =
-              convert_stan_interval(
-                results,
-                level = object$spec$method$confint$extras$level,
-                lower = FALSE
-              ),
-          )
-        if(object$spec$method$confint$extras$std_error)
-          res$.std_error <- apply(results, 2, sd, na.rm = TRUE)
-        res
-      },
-      func = c(pkg = "rstanarm", fun = "posterior_linpred"),
-      args =
-        list(
-          object = quote(object$fit),
-          newdata = quote(new_data),
-          transform = TRUE,
-          seed = expr(sample.int(10^5, 1))
-        )
-    ),
-    predint = list(
-      pre = NULL,
-      post = function(results, object) {
-        res <-
-          tibble(
-            .pred_lower =
-              convert_stan_interval(
-                results,
-                level = object$spec$method$predint$extras$level
-              ),
-            .pred_upper =
-              convert_stan_interval(
-                results,
-                level = object$spec$method$predint$extras$level,
-                lower = FALSE
-              ),
-          )
-        if(object$spec$method$predint$extras$std_error)
-          res$.std_error <- apply(results, 2, sd, na.rm = TRUE)
-        res
-      },
-      func = c(pkg = "rstanarm", fun = "posterior_predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newdata = quote(new_data),
-          seed = expr(sample.int(10^5, 1))
-        )
-    ),
-    raw = list(
-      pre = NULL,
-      func = c(fun = "predict"),
-      args =
-        list(
-          object = quote(object$fit),
-          newdata = quote(new_data)
-        )
     )
   )
 
-#' @importFrom  dplyr select rename
 linear_reg_spark_data <-
   list(
     libs = "sparklyr",
@@ -211,22 +67,5 @@ linear_reg_spark_data <-
       interface = "formula",
       protect = c("x", "formula", "weight_col"),
       func = c(pkg = "sparklyr", fun = "ml_linear_regression")
-    ),
-    pred = list(
-      pre = NULL,
-      post = function(results, object) {
-        results <- dplyr::rename(results, pred = prediction)
-        results <- dplyr::select(results, pred)
-        results
-      },
-      func = c(pkg = "sparklyr", fun = "ml_predict"),
-      args =
-        list(
-          x = quote(object$fit),
-          dataset = quote(new_data)
-        )
     )
   )
-
-
-
