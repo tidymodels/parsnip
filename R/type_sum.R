cc6cc45b382769a4eeac9b29af23dd8b298085e2
topepo <mxkuhn@gmail.com>
2018-09-28 22:50:07 -0400

    fixed type_sum args

--- R/type_sum.R
+++ R/type_sum.R
@@ -1,27 +1,27 @@
 #' Succinct summary of parsnip object
-#' 
+#'
 #' `type_sum` controls how objects are shown when inside tibble
-#'  columns. 
-#'  
-#' @details For `model_spec` objects, the summary is "`spec[?]`" 
-#'  or "`spec[+]`". The former indicates that either the model 
-#'  mode has not been declared or that the specification has 
-#'  `varying()` parameters. Otherwise, the latter is shown. 
-#'  
-#' For fitted models, either "`fit[x]`" or "`fit[+]`" are used 
+#'  columns.
+#' @param x	A `model_spec` or `model_fit` object to summarise.
+#' @details For `model_spec` objects, the summary is "`spec[?]`"
+#'  or "`spec[+]`". The former indicates that either the model
+#'  mode has not been declared or that the specification has
+#'  `varying()` parameters. Otherwise, the latter is shown.
+#'
+#' For fitted models, either "`fit[x]`" or "`fit[+]`" are used
 #'  where the "x" implies that the model fit failed in some way.
-#' @return A character value.  
-#' @importFrom pillar type_sum
+#' @return A character value.
+#' @importFrom tibble type_sum
 #' @method type_sum model_spec
 #' @export
-type_sum.model_spec <- function(x, ...) {
+type_sum.model_spec <- function(x) {
   resolved <- TRUE
-  if (x$mode == "unknown") 
+  if (x$mode == "unknown")
     resolved <- FALSE
   arg_info <- varying_args(x)
   if (any(arg_info$varying))
     resolved <- FALSE
-  
+
   res <- "spec"
   if (resolved) {
     res <- paste0(res, "[+]")
@@ -32,12 +32,12 @@ type_sum.model_spec <- function(x, ...) {
 }
 
 #' @rdname type_sum.model_spec
-#' @importFrom pillar type_sum
+#' @importFrom tibble type_sum
 #' @method type_sum model_fit
 #' @export
-type_sum.model_fit <- function(x, ...) {
+type_sum.model_fit <- function(x) {
   resolved <- TRUE
-  if (inherits(x$fit, "try-error")) 
+  if (inherits(x$fit, "try-error"))
     resolved <- FALSE
 
   res <- "fit"
