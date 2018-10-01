f746c21aef23ea461fe7c3d4efc0494ac91036ea
DavisVaughan <davis@rstudio.com>
2018-10-01 12:29:09 -0400

    Shift the call to get_method() in fit() to before the checks on package installs

--- R/fit.R
+++ R/fit.R
@@ -117,12 +117,12 @@ fit.model_spec <-
         "with a spark data object.", call. = FALSE
       )
 
+    # populate `method` with the details for this model type
+    object <- get_method(object, engine = object$engine)
+
     check_installs(object)  # TODO rewrite with pkgman
     # TODO Should probably just load the namespace
     load_libs(object, control$verbosity < 2)
-
-    # populate `method` with the details for this model type
-    object <- get_method(object, engine = object$engine)
 
     interfaces <- paste(fit_interface, object$method$fit$interface, sep = "_")
 
