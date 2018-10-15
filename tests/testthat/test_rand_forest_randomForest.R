d7c5bc364e6c9b6def93c964eaf13b3807212b3b
topepo <mxkuhn@gmail.com>
2018-10-15 12:10:03 -0400

    updated test for new quosure approach

--- tests/testthat/test_rand_forest_randomForest.R
+++ tests/testthat/test_rand_forest_randomForest.R
@@ -1,36 +1,41 @@
 library(testthat)
-context("random forest execution with randomForest")
 library(parsnip)
 library(tibble)
+
+# ------------------------------------------------------------------------------
+
+context("random forest execution with randomForest")
+
+# ------------------------------------------------------------------------------
 
 data("lending_club")
 lending_club <- head(lending_club, 200)
 num_pred <- c("funded_amnt", "annual_inc", "num_il_tl")
 
 lc_basic <- rand_forest(mode = "classification")
-bad_rf_cls <- rand_forest(mode = "classification",
-                          others = list(sampsize = -10))
+bad_rf_cls <- rand_forest(mode = "classification", sampsize = -10)
 
 ctrl <- fit_control(verbosity = 1, catch = FALSE)
 caught_ctrl <- fit_control(verbosity = 1, catch = TRUE)
 quiet_ctrl <- fit_control(verbosity = 0, catch = TRUE)
 
+# ------------------------------------------------------------------------------
 
 test_that('randomForest classification execution', {
 
   skip_if_not_installed("randomForest")
 
-  # passes interactively but not on R CMD check
-  # expect_error(
-  #   fit(
-  #     lc_basic,
-  #     Class ~ funded_amnt + term,
-  #     data = lending_club,
-  #     engine = "randomForest",
-  #     control = ctrl
-  #   ),
-  #   regexp = NA
-  # )
+  # check: passes interactively but not on R CMD check
+  expect_error(
+    fit(
+      lc_basic,
+      Class ~ funded_amnt + term,
+      data = lending_club,
+      engine = "randomForest",
+      control = ctrl
+    ),
+    regexp = NA
+  )
 
   expect_error(
     fit_xy(
@@ -46,22 +51,22 @@ test_that('randomForest classification execution', {
   expect_error(
     fit(
       bad_rf_cls,
-      unded_amnt ~ term,
+      funded_amnt ~ term,
       data = lending_club,
       engine = "randomForest",
       control = ctrl
     )
   )
 
-  # passes interactively but not on R CMD check
-  # randomForest_form_catch <- fit(
-  #   bad_rf_cls,
-  #   unded_amnt ~ term,
-  #   data = lending_club,
-  #   engine = "randomForest",
-  #   control = caught_ctrl
-  # )
-  # expect_true(inherits(randomForest_form_catch, "try-error"))
+  # check: passes interactively but not on R CMD check
+  randomForest_form_catch <- fit(
+    bad_rf_cls,
+    funded_amnt ~ term,
+    data = lending_club,
+    engine = "randomForest",
+    control = caught_ctrl
+  )
+  expect_true(inherits(randomForest_form_catch$fit, "try-error"))
 
   randomForest_xy_catch <- fit_xy(
     bad_rf_cls,
@@ -137,37 +142,37 @@ test_that('randomForest classification probabilities', {
 })
 
 
-###################################################################
+# ------------------------------------------------------------------------------
 
 car_form <- as.formula(mpg ~ .)
 num_pred <- names(mtcars)[3:6]
 
 car_basic <- rand_forest(mode = "regression")
 
-bad_ranger_reg <- rand_forest(mode = "regression",
-                              others = list(min.node.size = -10))
-bad_rf_reg <- rand_forest(mode = "regression",
-                          others = list(sampsize = -10))
+bad_ranger_reg <- rand_forest(mode = "regression", min.node.size = -10)
+bad_rf_reg <- rand_forest(mode = "regression", sampsize = -10)
 
 ctrl <- list(verbosity = 1, catch = FALSE)
 caught_ctrl <- list(verbosity = 1, catch = TRUE)
 quiet_ctrl <- list(verbosity = 0, catch = TRUE)
 
+# ------------------------------------------------------------------------------
+
 test_that('randomForest regression execution', {
 
   skip_if_not_installed("randomForest")
 
-  # passes interactively but not on R CMD check
-  # expect_error(
-  #   fit(
-  #     car_basic,
-  #     car_form,
-  #     data = mtcars,
-  #     engine = "randomForest",
-  #     control = ctrl
-  #   ),
-  #   regexp = NA
-  # )
+  # check: passes interactively but not on R CMD check
+  expect_error(
+    fit(
+      car_basic,
+      car_form,
+      data = mtcars,
+      engine = "randomForest",
+      control = ctrl
+    ),
+    regexp = NA
+  )
 
   expect_error(
     fit_xy(
@@ -180,15 +185,15 @@ test_that('randomForest regression execution', {
     regexp = NA
   )
 
-  # passes interactively but not on R CMD check
-  # randomForest_form_catch <- fit(
-  #   bad_rf_reg,
-  #   car_form,
-  #   data = mtcars,
-  #   engine = "randomForest",
-  #   control = caught_ctrl
-  # )
-  # expect_true(inherits(randomForest_form_catch, "try-error"))
+  # check: passes interactively but not on R CMD check
+  randomForest_form_catch <- fit(
+    bad_rf_reg,
+    car_form,
+    data = mtcars,
+    engine = "randomForest",
+    control = caught_ctrl
+  )
+  expect_true(inherits(randomForest_form_catch$fit, "try-error"))
 
   randomForest_xy_catch <- fit_xy(
     bad_rf_reg,
