c38a630c1dc1023bae82005396b26772e135bbec
Max Kuhn <mxkuhn@gmail.com>
2018-10-20 14:34:09 -0400

    survival model test cases

--- tests/testthat/test_surv_reg_flexsurv.R
+++ tests/testthat/test_surv_reg_flexsurv.R
@@ -13,15 +13,17 @@ ctrl <- fit_control(verbosity = 1, catch = FALSE)
 caught_ctrl <- fit_control(verbosity = 1, catch = TRUE)
 quiet_ctrl <- fit_control(verbosity = 0, catch = TRUE)
 
+# ------------------------------------------------------------------------------
+
 test_that('flexsurv execution', {
   skip_if_not_installed("flexsurv")
-  
+
   library(flexsurv)
   data(bc)
-  
+
   set.seed(4566)
   bc$group2 <- bc$group
-  
+
   expect_error(
     res <- fit(
       surv_basic,
@@ -51,4 +53,26 @@ test_that('flexsurv execution', {
       control = ctrl
     )
   )
+})
+
+test_that('flexsurv prediction', {
+  skip_if_not_installed("flexsurv")
+
+  library(flexsurv)
+  data(bc)
+
+  set.seed(4566)
+  bc$group2 <- bc$group
+
+  res <- fit(
+    surv_basic,
+    Surv(recyrs, censrec) ~ group,
+    data = bc,
+    control = ctrl,
+    engine = "flexsurv"
+  )
+  exp_pred <- summary(res$fit, head(bc), type = "mean")
+  exp_pred <- do.call("rbind", unclass(exp_pred))
+  exp_pred <- tibble(.pred = exp_pred$est)
+  expect_equal(exp_pred, predict(res, head(bc)))
 })
