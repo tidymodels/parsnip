97e8d2b63810553182a9f4d59ecd7a76f4f04692
Malcolm Barrett <malcolmbarrett@gmail.com>
2019-01-19 13:55:47 -0600

    add tidy() method for model_fit objects

---
+++ R/tidy.R
@@ -0,0 +1,22 @@
+#' Turn a parsnip model object into a tidy tibble
+#'
+#' This method tidies the model in a parsnip model object, if it exists.
+#'
+#' @inheritParams generics::tidy
+#'
+#' @return a tibble
+#' @export
+#'
+#' @examples
+#' logistic_reg() %>%
+#'   set_engine("glm") %>%
+#'   fit(Class ~ funded_amnt + int_rate, data = lending_club) %>%
+#'   # tidying model object and passing arguments to broom:::tidy.glm
+#'   tidy(conf.int = TRUE, exponentiate = TRUE)
+tidy.model_fit <- function(x, ...) {
+  tryCatch(generics::tidy(x$fit, ...),
+    error = function(error) {
+      cat("Error:", error$message)
+    }
+  )
+}
