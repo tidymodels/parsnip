2700f96f9d6779e9fbf55e3e1616c3b39e182473
topepo <mxkuhn@gmail.com>
2019-05-31 21:46:08 -0400

    more travis investigations

--- tests/testthat/test_surv_reg_survreg.R
+++ tests/testthat/test_surv_reg_survreg.R
@@ -19,7 +19,7 @@ quiet_ctrl <- fit_control(verbosity = 0, catch = TRUE)
 
 test_that('survival execution', {
 
-  skip()
+  skip("troubleshooting travis")
 
   expect_error(
     res <- fit(
@@ -50,7 +50,7 @@ test_that('survival execution', {
 })
 
 test_that('survival prediction', {
-  skip()
+  skip("troubleshooting travis")
 
   res <- fit(
     surv_basic,
