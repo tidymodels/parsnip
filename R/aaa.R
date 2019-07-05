82376cf7ab9e6375e43eacd4a9456c58288070e1
topepo <mxkuhn@gmail.com>
2019-07-05 16:53:22 -0400

    minor formatting

--- R/aaa.R
+++ R/aaa.R
@@ -1,9 +1,10 @@
 
 maybe_multivariate <- function(results, object) {
-  if (isTRUE(ncol(results) > 1))
+  if (isTRUE(ncol(results) > 1)) {
     results <- as_tibble(results)
-  else
+  }  else {
     results <- unname(results[, 1])
+  }
   results
 }
 
