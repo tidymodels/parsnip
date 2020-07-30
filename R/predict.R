57d3ca589edbca0b8a66b9ddc2c1607088269cd0
Max Kuhn <mxkuhn@gmail.com>
2020-07-30 11:13:21 -0400

    check for intercept before trying to remove

--- R/predict.R
+++ R/predict.R
@@ -244,7 +244,7 @@ prepare_data <- function(object, new_data) {
     get_encoding(class(object$spec)[1]) %>%
     dplyr::filter(mode == object$spec$mode, engine == object$spec$engine) %>%
     dplyr::pull(remove_intercept)
-  if (remove_intercept) {
+  if (remove_intercept & any(grepl("Intercept", names(new_data)))) {
     new_data <- new_data %>% dplyr::select(-dplyr::one_of("(Intercept)"))
   }
 
