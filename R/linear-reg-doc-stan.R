524cd49f09f10ad1ddee9d626f08ddf2dfbae09b
Max Kuhn <mxkuhn@gmail.com>
2021-03-22 17:59:12 -0400

    added stan details

---
+++ R/linear-reg-doc-stan.R
@@ -0,0 +1,9 @@
+#' Linear regression via Bayesian Methods
+#'
+#' The `stan` engine estimates regression parameters using Bayesian estimation.
+#'
+#' @includeRmd man/rmd/linear-reg-stan.Rmd details
+#'
+#' @name details-linear-reg-stan
+#' @keywords internal
+NULL
