9e6b912334ccadf8d5ab2a27691a4b61ffaf3e5a
Max Kuhn <mxkuhn@gmail.com>
2021-04-16 09:58:52 -0400

    use linked verions of function names

--- R/linear_reg_doc_glmnet.R
+++ R/linear_reg_doc_glmnet.R
@@ -1,6 +1,6 @@
 #' Linear regression via glmnet
 #'
-#' `glmnet()` uses regularized least squares to fit models with numeric outcomes.
+#' [glmnet::glmnet()] uses regularized least squares to fit models with numeric outcomes.
 #'
 #' @includeRmd man/rmd/linear_reg_glmnet.Rmd details
 #'
--- R/linear_reg_doc_spark.R
+++ R/linear_reg_doc_spark.R
@@ -1,6 +1,6 @@
 #' Linear regression via spark
 #'
-#' `sparklyr::ml_linear_regression()` uses regularized least squares to fit
+#' [sparklyr::ml_linear_regression()] uses regularized least squares to fit
 #' models with numeric outcomes.
 #'
 #' @includeRmd man/rmd/linear_reg_spark.Rmd details
