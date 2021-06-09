eea10fe480b75dc8ad8e005d2da9cd69887b16e8
Max Kuhn <mxkuhn@gmail.com>
2021-06-09 11:39:05 -0400

    updated glmnet information

--- R/glmnet_details.R
+++ R/glmnet_details.R
@@ -1,11 +1,7 @@
 #' Technical aspects of the glmnet model
 #'
-#' `glmnet` is a package for penalized regression models that encompasses
-#'  linear, logistic, Poisson, and multinomial regression models (as well as the
-#'  Cox proportional hazards model for censored data). It is a very popular model
-#'  and there are some nuances related to how it works that are important to
-#'  discuss. This discussion will focus on linear regression but the points
-#'  generalize to the other model types.
+#' glmnet is a popular statistical model for regularized generalized linear
+#' models. These notes reflect common questions about this particular model.
 #'
 #' @includeRmd man/rmd/glmnet-details.Rmd details
 #'
