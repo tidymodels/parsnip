be84cf71f91e48e1d74d77a432639428f7f88970
Hannah Frick <hannah@rstudio.com>
2021-11-18 11:34:14 +0000

    move methods for `tunable()` from tune to here

--- R/zzz.R
+++ R/zzz.R
@@ -23,6 +23,25 @@
     # `tune_args.model_spec()` moved from tune to parsnip
     vctrs::s3_register("generics::tune_args", "model_spec", tune_args_model_spec)
   }
+
+  should_register_tunable_method <- tryCatch(
+    expr = utils::packageVersion("tune") >= "0.1.6.9002",
+    error = function(cnd) TRUE
+  )
+
+  if (should_register_tunable_method) {
+    # `tunable.model_spec()` and friends moved from tune to parsnip
+    vctrs::s3_register("generics::tunable", "model_spec", tunable_model_spec)
+    vctrs::s3_register("generics::tunable", "linear_reg", tunable_linear_reg)
+    vctrs::s3_register("generics::tunable", "logistic_reg", tunable_logistic_reg)
+    vctrs::s3_register("generics::tunable", "multinomial_reg", tunable_multinomial_reg)
+    vctrs::s3_register("generics::tunable", "boost_tree", tunable_boost_tree)
+    vctrs::s3_register("generics::tunable", "rand_forest", tunable_rand_forest)
+    vctrs::s3_register("generics::tunable", "mars", tunable_mars)
+    vctrs::s3_register("generics::tunable", "decision_tree", tunable_decision_tree)
+    vctrs::s3_register("generics::tunable", "svm_poly", tunable_svm_poly)
+  }
+
 }
 
 
