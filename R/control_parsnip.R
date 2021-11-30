c42e804b7784cf1bfc0f6e64af04c50a7b2d0c8c
Julia Silge <julia.silge@gmail.com>
2021-11-30 12:13:46 -0700

    Edits to documentation

--- R/control_parsnip.R
+++ R/control_parsnip.R
@@ -1,20 +1,23 @@
 #' Control the fit function
 #'
-#' Options can be passed to the [fit.model_spec()] function that control the
+#' Pass options to the [fit.model_spec()] function to control its
 #' output and computations
 #'
-#' @param verbosity An integer where a value of zero indicates that no
-#'  messages or output should be shown when packages are loaded or when the
-#'  model is fit. A value of 1 means that package loading is quiet but model
+#' @param verbosity An integer to control how verbose the output is. For a
+#'  value of zero, no messages or output are shown when packages are loaded or
+#'  when the model is fit. For a value of 1, package loading is quiet but model
 #'  fits can produce output to the screen (depending on if they contain their
-#'  own `verbose`-type argument). Additionally, A value of 2 or more indicates
-#'  that any output should be seen and that the execution time of the fit should
-#'  be recorded and printed.
+#'  own `verbose`-type argument). For a value of 2 or more, any output at all
+#'  is displayed and the execution time of the fit is recorded and printed.
 #' @param catch A logical where a value of `TRUE` will evaluate the model
 #'  inside of `try(, silent = TRUE)`. If the model fails, an object is still
 #'  returned (without an error) that inherits the class "try-error".
-#' @return An S3 object with class "fit_control" that is a named list with the
-#' results of the function call
+#' @return An S3 object with class "control_parsnip" that is a named list
+#' with the results of the function call
+#'
+#' @examples
+#' control_parsnip(verbosity = 2L)
+#'
 #' @details
 #' `fit_control()` is deprecated in favor of `control_parsnip()`.
 #' @export
