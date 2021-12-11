e3d8e6e1efc3f95456e26548f7a6222b1360f954
Max Kuhn <mxkuhn@gmail.com>
2021-12-11 18:37:39 -0500

    new multilevelmod engine docs

---
+++ R/linear_reg_lme.R
@@ -0,0 +1,10 @@
+#' Linear regression via mixed models
+#'
+#' The `lme` engine estimates fixed and random effect regression parameters
+#' using maximum likelihood (or restricted maximum likelihood) estimation.
+#'
+#' @includeRmd man/rmd/linear_reg_lme.md details
+#'
+#' @name details_linear_reg_lme
+#' @keywords internal
+NULL
