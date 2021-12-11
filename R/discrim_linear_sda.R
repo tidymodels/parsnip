0dc7c76b5b8979ec276278c4c60458400214a2f9
Max Kuhn <mxkuhn@gmail.com>
2021-12-11 18:16:55 -0500

    add files for SDA model

---
+++ R/discrim_linear_sda.R
@@ -0,0 +1,10 @@
+#' Linear discriminant analysis via James-Stein-type shrinkage estimation
+#'
+#' [sda::sda()] can fit a linear discriminant analysis model that can fit models
+#' between classical discriminant analysis and diagonal discriminant analysis.
+#'
+#' @includeRmd man/rmd/discrim_linear_sda.md details
+#'
+#' @name details_discrim_linear_sda
+#' @keywords internal
+NULL
