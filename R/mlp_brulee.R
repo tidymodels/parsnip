546cded0a829f790e0237e801817f9d72db822ff
Max Kuhn <mxkuhn@gmail.com>
2021-12-17 14:43:00 -0500

    some brulee files

---
+++ R/mlp_brulee.R
@@ -0,0 +1,9 @@
+#' Multilayer perceptron via brulee
+#'
+#' [brulee::brulee_mlp()] fits a neural networks.
+#'
+#' @includeRmd man/rmd/mlp_brulee.md details
+#'
+#' @name details_mlp_brulee
+#' @keywords internal
+NULL
