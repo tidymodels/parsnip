4432811d89f94f1dc5d7ea96c4a5260f9cec0382
Max Kuhn <mxkuhn@gmail.com>
2022-01-04 11:51:08 -0500

    brulee engine files

---
+++ R/linear_reg_brulee.R
@@ -0,0 +1,10 @@
+#' Linear regression via brulee
+#'
+#' [brulee::brulee_linear_reg()] uses ordinary least squares to fit models with
+#' numeric outcomes.
+#'
+#' @includeRmd man/rmd/linear_reg_brulee.md details
+#'
+#' @name details_linear_reg_brulee
+#' @keywords internal
+NULL
---
+++ R/logistic_reg_brulee.R
@@ -0,0 +1,11 @@
+#' Logistic regression via brulee
+#'
+#' [brulee::brulee_logistic_reg()] fits a generalized linear model for binary
+#' outcomes. A linear combination of the predictors is used to model the log
+#' odds of an event.
+#'
+#' @includeRmd man/rmd/logistic_reg_brulee.md details
+#'
+#' @name details_logistic_reg_brulee
+#' @keywords internal
+NULL
