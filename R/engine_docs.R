bd9d0e32fefca18b97d3f01c2283c089fd155977
Max Kuhn <mxkuhn@gmail.com>
2022-02-01 14:32:29 -0500

    add issue summary post-knit

--- R/engine_docs.R
+++ R/engine_docs.R
@@ -1,6 +1,7 @@
 #' Knit engine-specific documentation
 #' @param pattern A regular expression to specify which files to knit. The
 #' default knits all engine documentation files.
+#' @param ... Options passed to [knitr::knit()].
 #' @return A tibble with column `file` for the file name and `result` (a
 #' character vector that echos the output file name or, when there is
 #' a failure, the error message).
@@ -13,13 +14,22 @@ knit_engine_docs <- function(pattern = NULL) {
     target_exists <- grepl(pattern, rmd_files)
     files <- rmd_files[target_exists]
   } else {
-    files <- rmd_files[!grepl("(template-)|(setup\\.)", rmd_files)]
+    files <- rmd_files[!grepl("(template-)|(setup\\.)|(aaa\\.)", rmd_files)]
   }
   outputs <- gsub("Rmd$", "md", files)
 
   res <- purrr::map2(files, outputs, ~ try(knitr::knit(.x, .y), silent = TRUE))
   res <- purrr::map_chr(res, as.character)
-  tibble::tibble(file = basename(files), result = res)
+
+  issues <- list_md_problems()
+  if (nrow(issues) > 0) {
+    cat("There are some issues with the help files:\n")
+    print(issues)
+  } else {
+    cat("No issues found in the help files.\n\n")
+  }
+
+  invisible(tibble::tibble(file = basename(files), result = res))
 }
 
 # ------------------------------------------------------------------------------
@@ -319,7 +329,7 @@ list_md_problems <- function() {
     lines <- readLines(file)
     line <- grep("## (Error|Warning)", lines)
     problem <- lines[line]
-    tibble(file, line, problem)
+    tibble(basename(file), line, problem)
   }
 
   purrr::map_dfr(md_files, get_errors)
