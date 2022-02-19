3d62ad06be6242242f8cf6ea073d29384c1059e5
Julia Silge <julia.silge@gmail.com>
2022-02-18 17:21:33 -0700

    Make nicer `fit()` link

--- man-roxygen/spec-details.R
+++ man-roxygen/spec-details.R
@@ -2,5 +2,5 @@
 #' This function only defines what _type_ of model is being fit. Once an engine
 #'  is specified, the _method_ to fit the model is also defined.
 #'
-#' The model is not trained or fit until the [fit.model_spec()] function is used
+#' The model is not trained or fit until the [`fit()`][fit.model_spec()] function is used
 #' with the data.
