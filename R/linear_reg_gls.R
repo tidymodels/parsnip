0e9f4bacee4309a05d3707c9305d9884eb92f9c0
Salim B <github@salim.space>
2022-03-30 00:17:11 +0000

    Fix typo (#695)
    * Fix typo
    
    * Redocument
    
    Co-authored-by: Julia Silge <julia.silge@gmail.com>

--- R/linear_reg_gls.R
+++ R/linear_reg_gls.R
@@ -1,7 +1,7 @@
 #' Linear regression via generalized least squares
 #'
 #' The `"gls"` engine estimates linear regression for models where the rows of the
-#' data are not indpendent.
+#' data are not independent.
 #'
 #' @includeRmd man/rmd/linear_reg_gls.md details
 #'
