312d191e1047eaf016f5c40c378ab626da77babc
Simon P. Couch <simonpatrickcouch@gmail.com>
2022-09-15 16:44:39 -0400

    unexport checking helpers (#809)

--- R/misc.R
+++ R/misc.R
@@ -54,43 +54,39 @@ mode_filter_condition <- function(mode, user_specified_mode) {
   rlang::quo(mode == !!mode)
 }
 
-#' @section Model Specification Checking:
-#'
-#' The helpers `spec_is_possible()`, `spec_is_loaded()`, and
-#' `prompt_missing_implementation()` provide tooling for checking
-#' model specifications. In addition to the `spec`, `engine`, and `mode`
-#' arguments, the functions take arguments `user_specified_engine` and
-#' `user_specified_mode`, denoting whether the user themselves has
-#' specified the engine or mode, respectively.
-#'
-#' `spec_is_possible()` checks against the union of
-#'
-#' * the current parsnip model environment and
-#' * the `model_info_table` of "pre-registered" model specifications
-#'
-#' to determine whether a model is well-specified. See
-#' `parsnip:::read_model_info_table()` for this table.
-#'
-#' `spec_is_loaded()` checks only against the current parsnip model environment.
-#'
-#' `spec_is_possible()` is executed automatically on `new_model_spec()`,
-#' `set_mode()`, and `set_engine()`, and `spec_is_loaded()` is executed
-#' automatically in `print.model_spec()`, among other places. `spec_is_possible()`
-#' should be used when a model specification is still "in progress" of being
-#' specified, while `spec_is_loaded` should only be called when parsnip or an
-#' extension receives some indication that the user is "done" specifying a model
-#' specification: at print, fit, addition to a workflow, or `extract_*()`, for
-#' example.
-#'
-#' When `spec_is_loaded()` is `FALSE`, the `prompt_missing_implementation()`
-#' helper will construct an informative message to prompt users to load or
-#' install needed packages. It's `prompt` argument refers to the prompting
-#' function to use, usually [cli::cli_inform] or [cli::cli_abort], and the
-#' ellipses are passed to that function.
-#'
-#' @export
-#' @keywords internal
-#' @rdname add_on_exports
+# Model Specification Checking:
+#
+# The helpers `spec_is_possible()`, `spec_is_loaded()`, and
+# `prompt_missing_implementation()` provide tooling for checking
+# model specifications. In addition to the `spec`, `engine`, and `mode`
+# arguments, the functions take arguments `user_specified_engine` and
+# `user_specified_mode`, denoting whether the user themselves has
+# specified the engine or mode, respectively.
+#
+# `spec_is_possible()` checks against the union of
+#
+# * the current parsnip model environment and
+# * the `model_info_table` of "pre-registered" model specifications
+#
+# to determine whether a model is well-specified. See
+# `parsnip:::read_model_info_table()` for this table.
+#
+# `spec_is_loaded()` checks only against the current parsnip model environment.
+#
+# `spec_is_possible()` is executed automatically on `new_model_spec()`,
+# `set_mode()`, and `set_engine()`, and `spec_is_loaded()` is executed
+# automatically in `print.model_spec()`, among other places. `spec_is_possible()`
+# should be used when a model specification is still "in progress" of being
+# specified, while `spec_is_loaded` should only be called when parsnip or an
+# extension receives some indication that the user is "done" specifying a model
+# specification: at print, fit, addition to a workflow, or `extract_*()`, for
+# example.
+#
+# When `spec_is_loaded()` is `FALSE`, the `prompt_missing_implementation()`
+# helper will construct an informative message to prompt users to load or
+# install needed packages. It's `prompt` argument refers to the prompting
+# function to use, usually [cli::cli_inform] or [cli::cli_abort], and the
+# ellipses are passed to that function.
 spec_is_possible <- function(spec,
                              engine = spec$engine,
                              user_specified_engine = spec$user_specified_engine,
@@ -119,10 +115,7 @@ spec_is_possible <- function(spec,
   return(nrow(possibilities) > 0)
 }
 
-# see ?add_on_exports for more information on usage
-#' @export
-#' @keywords internal
-#' @rdname add_on_exports
+# see notes above spec_is_possible for more information on usage
 spec_is_loaded <- function(spec,
                            engine = spec$engine,
                            user_specified_engine = spec$user_specified_engine,
@@ -160,10 +153,7 @@ is_printable_spec <- function(x) {
 # if there's a "pre-registered" extension supporting that setup,
 # nudge the user to install/load it.
 #
-# see ?add_on_exports for more information on usage
-#' @export
-#' @keywords internal
-#' @rdname add_on_exports
+# see notes above spec_is_possible for more information on usage
 prompt_missing_implementation <- function(spec,
                                           engine = spec$engine,
                                           user_specified_engine = spec$user_specified_engine,
