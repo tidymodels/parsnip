00cd2c2251330ab18887876f578fbbb87d9638c4
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2022-09-15 11:31:44 -0700

    Add condense_control() function (#808)
    * add condense_control function
    
    * add tests for condense_control
    
    * use condense_control function
    
    * update news about condense_control
    
    * correct spacing
    
    * add missing updated test
    
    * add reminder to add arguments to other control functions

---
+++ tests/testthat/test_condense_control.R
@@ -0,0 +1,21 @@
+test_that("condense_control works", {
+  ctrl <- control_parsnip()
+
+  expect_equal(
+    condense_control(ctrl, ctrl),
+    ctrl
+  )
+
+  ctrl$allow_par <- TRUE
+  ctrl$catch <- TRUE
+
+  expect_equal(
+    condense_control(ctrl, control_parsnip()),
+    control_parsnip(catch = TRUE)
+  )
+
+  ctrl$anotherone <- 2
+  expect_snapshot(error = TRUE,
+    condense_control(control_parsnip(), ctrl)
+  )
+})
