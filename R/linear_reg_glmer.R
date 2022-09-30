3921f5188fc4aba9bdee9dff4d285587e628603c
Max Kuhn <mxkuhn@gmail.com>
2022-09-30 11:17:29 -0400

    engine-specific docs for the glmer engine of linear_reg() (#821)
    * add glmer engine doc
    
    * update docs

---
+++ R/linear_reg_glmer.R
@@ -0,0 +1,12 @@
+#' Linear regression via generalized mixed models
+#'
+#' The `"glmer"` engine estimates fixed and random effect regression parameters
+#' using maximum likelihood (or restricted maximum likelihood) estimation.
+#'
+#' @includeRmd man/rmd/linear_reg_glmer.md details
+#'
+#' @name details_linear_reg_glmer
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
