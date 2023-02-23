453387f41fe14e747c901f1335eeb658a55fb09e
Hannah Frick <hannah@rstudio.com>
2023-02-23 18:53:10 +0000

    link out to more context

--- R/proportional_hazards.R
+++ R/proportional_hazards.R
@@ -86,6 +86,7 @@ translate.proportional_hazards <- function(x, engine = x$engine, ...) {
   x <- translate.default(x, engine, ...)
 
   if (engine == "glmnet") {
+    # See https://parsnip.tidymodels.org/reference/glmnet-details.html
     .check_glmnet_penalty_fit(x)
     if (any(names(x$eng_args) == "path_values")) {
       # Since we decouple the parsnip `penalty` argument from being the same
