1928dce64a612c3879fb303ecc1ab0ae2fef739c
topepo <mxkuhn@gmail.com>
2023-03-01 14:11:34 -0500

    updates from reviewer feedback

--- R/standalone-survival.R
+++ R/standalone-survival.R
@@ -52,7 +52,10 @@
   .is_surv(surv)
   res <-   surv[, "status"]
   un_vals <- sort(unique(res))
-  if (identical(un_vals, 1:2) | identical(un_vals, c(1.0, 2.0))) {
+  event_type_to_01 <- !(.extract_surv_type(surv) %in% c("interval", "interval2", "mstate"))
+  if (
+    event_type_to_01 &&
+    (identical(un_vals, 1:2) | identical(un_vals, c(1.0, 2.0))) ) {
     res <- res - 1
   }
   res
