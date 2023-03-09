21fc519e9a02c43e106a6a7848beba6e6bba88fe
Simon P. Couch <simonpatrickcouch@gmail.com>
2023-03-09 08:29:32 -0500

    `read_model_info_table()` only once (#910)

--- R/misc.R
+++ R/misc.R
@@ -69,7 +69,7 @@ mode_filter_condition <- function(mode, user_specified_mode) {
 #' * the `model_info_table` of "pre-registered" model specifications
 #'
 #' to determine whether a model is well-specified. See
-#' `parsnip:::read_model_info_table()` for this table.
+#' `parsnip:::model_info_table` for this table.
 #'
 #' `spec_is_loaded()` checks only against the current parsnip model environment.
 #'
@@ -99,7 +99,7 @@ spec_is_possible <- function(spec,
 
   all_model_info <-
     dplyr::full_join(
-      read_model_info_table(),
+      model_info_table,
       rlang::env_get(get_model_env(), cls) %>% dplyr::mutate(model = cls),
       by = c("model", "engine", "mode")
     )
@@ -183,7 +183,7 @@ prompt_missing_implementation <- function(spec,
   }
 
   all <-
-    read_model_info_table() %>%
+    model_info_table %>%
     dplyr::filter(model == cls, !!mode_condition, !!engine_condition, !is.na(pkg)) %>%
     dplyr::select(-model)
 
