cbeda777861713d577cfeb3be2f613ebc7bea259
Hannah Frick <hannah@rstudio.com>
2023-03-13 13:47:36 +0000

    register `autoplot()` methods the regular way
    since parsnip imports ggplot2

--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-# nocov start
-
-.onLoad <- function(libname, pkgname) {
-  vctrs::s3_register("ggplot2::autoplot", "model_fit")
-  vctrs::s3_register("ggplot2::autoplot", "glmnet")
-}
-
-# nocov end
