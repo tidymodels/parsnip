b64f30fe895edfd485cae4e6dd112903ce17387b
Hannah Frick <hfrick@users.noreply.github.com>
2023-03-23 13:41:08 +0000

    `time` to `eval_time` (#936)
    * reformat
    
    * rename `time` to `eval_time`

--- R/predict_survival.R
+++ R/predict_survival.R
@@ -4,35 +4,48 @@
 #' @method predict_survival model_fit
 #' @export predict_survival.model_fit
 #' @export
-predict_survival.model_fit <-
-  function(object, new_data, time, interval = "none", level = 0.95, ...) {
-
-    check_spec_pred_type(object, "survival")
-
-    if (inherits(object$fit, "try-error")) {
-      rlang::warn("Model fit failed; cannot make predictions.")
-      return(NULL)
-    }
-
-    new_data <- prepare_data(object, new_data)
-
-    # preprocess data
-    if (!is.null(object$spec$method$pred$survival$pre))
-      new_data <- object$spec$method$pred$survival$pre(new_data, object)
-
-    # Pass some extra arguments to be used in post-processor
-    object$spec$method$pred$survival$args$time <- time
-    pred_call <- make_pred_call(object$spec$method$pred$survival)
-
-    res <- eval_tidy(pred_call)
-
-    # post-process the predictions
-    if(!is.null(object$spec$method$pred$survival$post)) {
-      res <- object$spec$method$pred$survival$post(res, object)
-    }
-
-    res
+predict_survival.model_fit <- function(object,
+                                       new_data,
+                                       eval_time,
+                                       time = deprecated(),
+                                       interval = "none",
+                                       level = 0.95,
+                                       ...) {
+  if (lifecycle::is_present(time)) {
+    lifecycle::deprecate_warn(
+      "1.0.4.9005",
+      "predict_survival(time)",
+      "predict_survival(eval_time)"
+    )
+    eval_time <- time
   }
+
+  check_spec_pred_type(object, "survival")
+
+  if (inherits(object$fit, "try-error")) {
+    rlang::warn("Model fit failed; cannot make predictions.")
+    return(NULL)
+  }
+
+  new_data <- prepare_data(object, new_data)
+
+  # preprocess data
+  if (!is.null(object$spec$method$pred$survival$pre))
+    new_data <- object$spec$method$pred$survival$pre(new_data, object)
+
+  # Pass some extra arguments to be used in post-processor
+  object$spec$method$pred$survival$args$eval_time <- eval_time
+  pred_call <- make_pred_call(object$spec$method$pred$survival)
+
+  res <- eval_tidy(pred_call)
+
+  # post-process the predictions
+  if(!is.null(object$spec$method$pred$survival$post)) {
+    res <- object$spec$method$pred$survival$post(res, object)
+  }
+
+  res
+}
 
 #' @export
 #' @keywords internal
