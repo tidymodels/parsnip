dfcca77dbfc85e8e4024fd81a3404a69f5714ff3
Hannah Frick <hannah@rstudio.com>
2023-03-24 15:12:19 +0000

    no need to fiddle with call
    (could be `NULL` but not `FALSE`)

--- R/ipcw.R
+++ R/ipcw.R
@@ -189,7 +189,7 @@ graf_weight_time_vec <- function(surv_obj, eval_time, eps = 10^-10) {
                                              cens_predictors = NULL,
                                              trunc = 0.05, eps = 10^-10, ...) {
   if (is.null(object$fit$fit)) {
-    rlang::abort("The workflow does not have a model fit object.", call = FALSE)
+    rlang::abort("The workflow does not have a model fit object.")
   }
   .censoring_weights_graf(object$fit$fit, predictions, cens_predictors, trunc, eps)
 }
