439a9fe2fc594e7838d60b2ef8539febdbeee976
Hannah Frick <hannah@rstudio.com>
2023-05-15 13:49:37 +0100

    refactor tests for `trunc_probs()`

--- tests/testthat/test-survival-censoring-weights.R
+++ tests/testthat/test-survival-censoring-weights.R
@@ -1,23 +1,28 @@
-test_that('probability truncation', {
+test_that("probability truncation via trunc_probs()", {
   probs <- seq(0, 1, length.out = 5)
 
-  expect_equal(
-    min(parsnip:::trunc_probs(probs, .4)),
-    min(probs[probs > 0]) / 2
-  )
-  expect_equal(
-    min(parsnip:::trunc_probs(c(NA, probs), .4), na.rm = TRUE),
-    min(probs[probs > 0]) / 2
-  )
-  expect_equal(
-    min(parsnip:::trunc_probs(probs)),
-    0.01
-  )
-  expect_equal(
-    min(parsnip:::trunc_probs((1:200)/200)),
-    1 / 200
-  )
+  probs_trunc_001 <- parsnip:::trunc_probs(probs, trunc = 0.01)
+  expect_equal(probs_trunc_001[1], 0.01)
+  expect_equal(probs_trunc_001[2:5], probs[2:5])
 
+  probs_trunc_04 <- parsnip:::trunc_probs(probs, trunc = 0.4)
+  data_derived_trunc <- min(probs[probs > 0]) / 2
+  expect_equal(probs_trunc_04[1], data_derived_trunc)
+  expect_equal(probs_trunc_04[2:5], probs[2:5])
+
+  probs_trunc_04_na <- parsnip:::trunc_probs(c(NA, probs), 0.4)
+  expect_identical(probs_trunc_04_na[1], NA_real_)
+  expect_equal(probs_trunc_04_na[2], data_derived_trunc)
+  expect_equal(probs_trunc_04_na[3:6], probs[2:5])
+
+  probs <- (1:200)/200
+  expect_identical(
+    parsnip:::trunc_probs(probs, trunc = 0.01),
+    probs
+  )
+})
+
+test_that("trunc_probs()", {
   probs_1 <- (0:10) / 20
   probs_2 <- probs_1
   probs_2[3] <- NA_real_
