9afb8b488ba8d2e0dd5945c72507f6550dbff684
Hannah Frick <hfrick@users.noreply.github.com>
2023-07-18 16:32:43 +0100

    remove `.time_as_binary_event()` (#980)
    * remove `.time_as_binary_event()`
    
    until we have a better understanding of the use cases to inform how we deal with not-well-defined evaluation times
    
    * update NEWS

--- R/survival-helpers.R
+++ R/survival-helpers.R
@@ -24,28 +24,3 @@ assign(".extract_surv_time", surv_ns[[".extract_surv_time"]])
 #' @return A numeric vector.
 #' @export
 assign(".extract_surv_status", surv_ns[[".extract_surv_status"]])
-
-#' Convert survival objects to binary factors
-#'
-#' For a given evaluation time, convert a [survival::Surv()] object to a binary
-#' factor with levels `"event"` and `"non-event"`.
-#'
-#' @name .time_as_binary_event
-#' @param surv A single [survival::Surv()] object.
-#' @param eval_time A single numeric value for the evaluation time.
-#' @return A two level factor.
-#' @details
-#' The following three cases can occur:
-#'  - **Events**: Evaluation time is greater than or equal to the event time
-#' ("it has already happened").
-#'  - **Non-events**: Evaluation time is less than the observed time, censored
-#'  or not ("nothing has happened yet").
-#'  - **Ambiguous outcomes**: Evaluation time is greater than or equal to the
-#'  observed censored time ("we don't know if anything might have happened by now").
-#'  A missing value is returned for these observations.
-#'
-#' @references  Graf, E., Schmoor, C., Sauerbrei, W., and Schumacher, M. (1999).
-#' "Assessment and Comparison of Prognostic Classification Schemes for Survival Data."
-#' _Statistics in Medicine_, 18, 2529-2545.
-#' @export
-assign(".time_as_binary_event", surv_ns[[".time_as_binary_event"]])
