e5ab565c0c369bdafb0b6f9d074be56698ba7b66
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-12-12 15:07:26 -0800

    remove .censoring_weights_graf.workflow

--- R/survival-censoring-weights.R
+++ R/survival-censoring-weights.R
@@ -178,19 +178,6 @@ graf_weight_time_vec <- function(surv_obj, eval_time, eps = 10^-10) {
   rlang::abort(msg)
 }
 
-
-#' @export
-#' @rdname censoring_weights
-.censoring_weights_graf.workflow <- function(object,
-                                             predictions,
-                                             cens_predictors = NULL,
-                                             trunc = 0.05, eps = 10^-10, ...) {
-  if (is.null(object$fit$fit)) {
-    rlang::abort("The workflow does not have a model fit object.")
-  }
-  .censoring_weights_graf(object$fit$fit, predictions, cens_predictors, trunc, eps)
-}
-
 #' @export
 #' @rdname censoring_weights
 .censoring_weights_graf.model_fit <- function(object,
