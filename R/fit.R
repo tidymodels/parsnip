9ceac0d7c8d9ac30cb86952790ef11e3dcb9e2ec
simonpcouch <simonpatrickcouch@gmail.com>
2024-02-12 10:04:21 -0600

    remove now-redundant class check

--- R/fit.R
+++ R/fit.R
@@ -411,10 +411,8 @@ check_xy_interface <- function(x, y, cl, model) {
     inher(x, c("data.frame", "matrix"), cl)
   }
 
-  # `y` can be a vector (which is not a class), or a factor or
-  # Surv object (which are not vectors)
   if (!is.null(y) && !is.atomic(y))
-    inher(y, c("data.frame", "matrix", "factor", "Surv"), cl)
+    inher(y, c("data.frame", "matrix"), cl)
 
   # rule out spark data sets that don't use the formula interface
   if (inherits(x, "tbl_spark") | inherits(y, "tbl_spark"))
