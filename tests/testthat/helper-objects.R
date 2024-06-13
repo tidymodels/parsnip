44f2f8189ba52bfbf7407967c52adc0a73df3a2b
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-06-12 22:04:59 -0700

    add sparse_hotel_rates() helper function

--- tests/testthat/helper-objects.R
+++ tests/testthat/helper-objects.R
@@ -24,3 +24,30 @@ is_tf_ok <- function() {
   }
   res
 }
+
+# ------------------------------------------------------------------------------
+# For sparse tibble testing
+
+sparse_hotel_rates <- function() {
+  # 99.2 sparsity
+  hotel_rates <- modeldata::hotel_rates
+
+  prefix_colnames <- function(x, prefix) {
+    colnames(x) <- paste(colnames(x), prefix, sep = "_")
+    x
+  }
+
+  dummies_country <- hardhat::fct_encode_one_hot(hotel_rates$country)
+  dummies_company <- hardhat::fct_encode_one_hot(hotel_rates$company)
+  dummies_agent <- hardhat::fct_encode_one_hot(hotel_rates$agent)
+
+  res <- dplyr::bind_cols(
+    hotel_rates["avg_price_per_room"],
+    prefix_colnames(dummies_country, "country"),
+    prefix_colnames(dummies_company, "company"),
+    prefix_colnames(dummies_agent, "agent")
+  )
+
+  res <- as.matrix(res)
+  Matrix::Matrix(res, sparse = TRUE)
+}
