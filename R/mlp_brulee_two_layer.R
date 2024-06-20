eede3b766d9ff7c77414a91491c243ce800b5ff3
‘topepo’ <mxkuhn@gmail.com>
2024-06-20 10:23:24 -0400

    engine work for two-layer mlp models

---
+++ R/mlp_brulee_two_layer.R
@@ -0,0 +1,11 @@
+#' Two-layer perceptron via brulee
+#'
+#' [brulee::brulee_mlp_two_layer()] fits a neural network.
+#'
+#' @includeRmd man/rmd/mlp_brulee_two_layer.md details
+#'
+#' @name details_mlp_brulee_two_layer
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
