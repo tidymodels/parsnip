e0e0dc81f08f97f10422999320f2c48fd6c28910
simonpcouch <simonpatrickcouch@gmail.com>
2024-08-28 09:14:30 -0500

    pass calls, use inline markup

--- R/predict.R
+++ R/predict.R
@@ -191,41 +191,69 @@ predict.model_fit <- function(object, new_data, type = NULL, opts = list(), ...)
   res
 }
 
-check_pred_type <- function(object, type, ...) {
+check_pred_type <- function(object, type, ..., call = rlang::caller_env()) {
   if (is.null(type)) {
     type <-
-      switch(object$spec$mode,
-             regression = "numeric",
-             classification = "class",
-             "censored regression" = "time",
-             cli::cli_abort("{.arg type} should be 'regression', 'censored regression', or 'classification'."))
+      switch(
+        object$spec$mode,
+        regression = "numeric",
+        classification = "class",
+        "censored regression" = "time",
+        cli::cli_abort(
+          "{.arg type} should be 'regression', 'censored regression', or 'classification'.",
+          call = call
+        )
+      )
   }
   if (!(type %in% pred_types))
     cli::cli_abort(
-        "{.arg type} should be one of:{.arg {pred_types}}")
+      "{.arg type} should be one of:{.arg {pred_types}}",
+      call = call
+    )
 
   switch(
     type,
     "numeric" = if (object$spec$mode != "regression") {
-      cli::cli_abort("For numeric predictions, the object should be a regression model.")
+      cli::cli_abort(
+        "For numeric predictions, the object should be a regression model.",
+        call = call
+      )
     },
     "class" = if (object$spec$mode != "classification") {
-      cli::cli_abort("For class predictions, the object should be a classification model.")
+      cli::cli_abort(
+        "For class predictions, the object should be a classification model.",
+        call = call
+      )
     },
     "prob" = if (object$spec$mode != "classification") {
-      cli::cli_abort("For probability predictions, the object should be a classification model.")
+      cli::cli_abort(
+        "For probability predictions, the object should be a classification model.",
+        call = call
+      )
     },
     "time" = if (object$spec$mode != "censored regression") {
-      cli::cli_abort("For event time predictions, the object should be a censored regression.")
+      cli::cli_abort(
+        "For event time predictions, the object should be a censored regression.",
+        call = call
+      )
     },
     "survival" = if (object$spec$mode != "censored regression") {
-      cli::cli_abort("For survival probability predictions, the object should be a censored regression.")
+      cli::cli_abort(
+        "For survival probability predictions, the object should be a censored regression.",
+        call = call
+      )
     },
     "hazard" = if (object$spec$mode != "censored regression") {
-      cli::cli_abort("For hazard predictions, the object should be a censored regression.")
+      cli::cli_abort(
+        "For hazard predictions, the object should be a censored regression.",
+        call = call
+      )
     },
     "linear_pred" = if (object$spec$mode != "censored regression") {
-      cli::cli_abort("For the linear predictor, the object should be a censored regression.")
+      cli::cli_abort(
+        "For the linear predictor, the object should be a censored regression.",
+        call = call
+      )
     }
   )
 
@@ -363,16 +391,19 @@ check_pred_type_dots <- function(object, type, ..., call = rlang::caller_env()) 
   # places where eval_time should not be given
   if (any(nms == "eval_time") & !type %in% c("survival", "hazard")) {
     cli::cli_abort(
-      "{.arg eval_time} should only be passed to {.fn predict} when \\ 
-       {.arg type} is one of {.or {.val {eval_time_types}}}",
+      "{.arg eval_time} should only be passed to {.fn predict} when \\
+       {.arg type} is one of {.or {.val {eval_time_types}}}.",
        call = call
      )
 
 
   }
   if (any(nms == "time") & !type %in% c("survival", "hazard")) {
-    cli::cli_abort("{.arg time} should only be passed to {.fn predict} when {.arg type} is one of:
-       {.val {eval_time_types}}")
+    cli::cli_abort(
+      "{.arg time} should only be passed to {.fn predict} when {.arg type} is
+       one of {.or {.val {eval_time_types}}}.",
+      call = call
+    )
   }
   # when eval_time should be passed
   if (!any(nms %in% c("eval_time", "time")) & type %in% c("survival", "hazard")) {
@@ -388,10 +419,10 @@ check_pred_type_dots <- function(object, type, ..., call = rlang::caller_env()) 
       !(type == "linear_pred" &
         object$spec$mode == "censored regression")) {
     cli::cli_abort(
-        "{.arg increasing} only applies to predictions of
-         type 'linear_pred' for the mode censored regression.",
-         call = call
-      )
+      "{.arg increasing} only applies to predictions of
+       type 'linear_pred' for the mode censored regression.",
+      call = call
+    )
 
   }
 
