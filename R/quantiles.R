5f6db8caf7bf8c9373c7eda2d5a141b90155e84c
‘topepo’ <mxkuhn@gmail.com>
2024-08-29 16:05:34 -0400

    add quantile_level argument to set_mode()

---
+++ R/quantiles.R
@@ -0,0 +1,21 @@
+# Helpers for quantile regression models
+
+check_quantile_level <- function(x, object, call) {
+  if ( object$mode != "quantile regression" ) {
+    return(invisible(TRUE))
+  } else {
+    if ( is.null(x) ) {
+      cli::cli_abort("In {.fn check_mode}, at least one value of
+      {.arg quantile_level} must be specified for quantile regression models.")
+    }
+  }
+  res <-
+    purrr::map(x,
+               ~ check_number_decimal(.x, min = 0, max = 1,
+                                      arg = "quantile_level", call = call,
+                                      allow_infinite = FALSE)
+  )
+  return(invisible(TRUE))
+}
+
+
