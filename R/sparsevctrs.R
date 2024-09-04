70274565852ef8f86e9480fc333173bfe7d2271a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2024-09-04 14:44:46 -0700

    make allow_sparse() work with model_fit

--- R/sparsevctrs.R
+++ R/sparsevctrs.R
@@ -3,6 +3,10 @@ to_sparse_data_frame <- function(x, object) {
     if (allow_sparse(object)) {
       x <- sparsevctrs::coerce_to_sparse_data_frame(x)
     } else {
+      if (inherits(object, "model_fit")) {
+        object <- object$spec
+      }
+    
       cli::cli_abort(
       "{.arg x} is a sparse matrix, but {.fn {class(object)[1]}} with
        engine {.code {object$engine}} doesn't accept that.")
