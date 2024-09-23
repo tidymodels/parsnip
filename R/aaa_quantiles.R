41ff0f8661303decb9d01d6d1c3cfe060d0b2202
‘topepo’ <mxkuhn@gmail.com>
2024-09-23 18:06:20 -0400

    export function for censored to use

--- R/aaa_quantiles.R
+++ R/aaa_quantiles.R
@@ -1,3 +1,10 @@
+#' Reformat quantile predictions
+#'
+#' @param x A matrix of predictions with rows as samples and columns as quantile
+#' levels.
+#' @param object A parsnip `model_fit` object from a quantile regression model.
+#' @keywords internal
+#' @export
 matrix_to_quantile_pred <- function(x, object) {
   if (!is.matrix(x)) {
     x <- as.matrix(x)
