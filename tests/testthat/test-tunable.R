c5c3f6de1aec2454aedd802e424f51d467f47a8a
‘topepo’ <mxkuhn@gmail.com>
2025-01-28 07:52:34 -0500

    fixes for #1236

---
+++ tests/testthat/test-tunable.R
@@ -0,0 +1,22 @@
+test_that('brulee has mixture object', {
+  # for issue 1236
+  mlp_spec <-
+    mlp(
+      hidden_units = tune(),
+      activation =  tune(),
+      penalty = tune(),
+      learn_rate = tune(),
+      epoch = 2000
+    ) %>%
+    set_mode("regression") %>%
+    set_engine("brulee",
+               stop_iter = tune(),
+               mixture = tune(),
+               rate_schedule = tune())
+
+  brulee_res <- tunable(mlp_spec)
+
+  expect_true(
+    length(brulee_res$call_info[brulee_res$name == "mixture"]) > 0
+  )
+})
