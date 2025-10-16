508f8bb5e1367a1bd9ca51c59f21b87885f5773b
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2025-10-16 14:43:56 -0400

    add minimal rmd docs for new engines

---
+++ R/ordinal_reg_vgam.R
@@ -0,0 +1,12 @@
+#' Ordinal regression via vector GAMs
+#'
+#' [VGAM::vgam()] fits vector generalized additive models, which specialize to
+#' several families of ordinal regression models.
+#'
+#' @includeRmd man/rmd/ordinal_reg_vgam.md details
+#'
+#' @name details_ordinal_reg_vgam
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed.
