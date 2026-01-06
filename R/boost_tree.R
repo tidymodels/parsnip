bf4e28262a5f6c3e80aa28b4d16de2393be5ef36
Emil Hvitfeldt <emil.hvitfeldt@posit.co>
2026-01-06 08:04:21 -0800

    stop out of bounds `iterationrange` for xgboost (#1317)

--- R/boost_tree.R
+++ R/boost_tree.R
@@ -653,16 +653,19 @@ multi_predict._xgb.Booster <-
 xgb_by_tree <- function(tree, object, new_data, type, ...) {
   rlang::check_installed("xgboost")
   if (utils::packageVersion("xgboost") >= "2.0.0.0") {
+    # avoid out of range
+    range_end <- min(nrow(attr(object$fit, "evaluation_log")), tree + 1)
+
     pred <- xgb_predict(
       object$fit,
       new_data = new_data,
-      iterationrange = c(1, tree + 1)
+      iterationrange = c(1, range_end)
     )
   } else {
     pred <- xgb_predict(
       object$fit,
       new_data = new_data,
-      iterationrange = c(1, tree + 1),
+      iterationrange = c(1, min(object$fit$niter, tree + 1)),
       ntreelimit = NULL
     )
   }
