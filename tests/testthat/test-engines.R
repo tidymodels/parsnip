c39b3e05db5097202a415ba6f1117b36f12397bf
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-03-02 11:30:29 -0800

    move test around to make sure it triggers

--- tests/testthat/test-engines.R
+++ tests/testthat/test-engines.R
@@ -7,14 +7,3 @@ test_that("NULL engines", {
   )
 })
 
-test_that("misspelled engine names give informative error", {
-  # See issue https://github.com/tidymodels/parsnip/issues/1110
-  skip_if_not_installed("poissonreg")
-
-  expect_snapshot(
-    poisson_reg() |>
-      set_engine("gml") |>
-      set_mode("regression"),
-    error = TRUE
-  )
-})
