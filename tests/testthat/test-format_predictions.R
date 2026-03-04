7a2e55e111e9a420603eda9d111936caf1d3d4c1
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-03-04 09:16:31 -0800

    use expect_identical() for all format_predictions() tests

--- tests/testthat/test-format_predictions.R
+++ tests/testthat/test-format_predictions.R
@@ -1,92 +1,70 @@
 test_that("format_predictions() handles numeric type", {
   x <- c(1.5, 2.5, 3.5)
-  res <- format_predictions(x, "numeric")
+  exp <- tibble::tibble(.pred = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred")
-  expect_equal(res$.pred, x)
+  expect_identical(format_predictions(x, "numeric"), exp)
 })
 
 test_that("format_predictions() handles class type", {
   x <- factor(c("a", "b", "a"))
-  res <- format_predictions(x, "class")
+  exp <- tibble::tibble(.pred_class = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred_class")
-  expect_equal(res$.pred_class, x)
+  expect_identical(format_predictions(x, "class"), exp)
 })
 
 test_that("format_predictions() handles prob type", {
   x <- data.frame(a = c(0.2, 0.3), b = c(0.8, 0.7))
-  res <- format_predictions(x, "prob")
+  exp <- tibble::tibble(.pred_a = c(0.2, 0.3), .pred_b = c(0.8, 0.7))
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, c(".pred_a", ".pred_b"))
-  expect_equal(res$.pred_a, x$a)
-  expect_equal(res$.pred_b, x$b)
+  expect_identical(format_predictions(x, "prob"), exp)
 })
 
 test_that("format_predictions() handles time type", {
   x <- c(10, 20, 30)
-  res <- format_predictions(x, "time")
+  exp <- tibble::tibble(.pred_time = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred_time")
-  expect_equal(res$.pred_time, x)
+  expect_identical(format_predictions(x, "time"), exp)
 })
 
 test_that("format_predictions() handles survival type", {
   x <- c(0.9, 0.8, 0.7)
-  res <- format_predictions(x, "survival")
+  exp <- tibble::tibble(.pred = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred")
-  expect_equal(res$.pred, x)
+  expect_identical(format_predictions(x, "survival"), exp)
 })
 
 test_that("format_predictions() handles linear_pred type", {
   x <- c(-1, 0, 1)
-  res <- format_predictions(x, "linear_pred")
+  exp <- tibble::tibble(.pred_linear_pred = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred_linear_pred")
-  expect_equal(res$.pred_linear_pred, x)
+  expect_identical(format_predictions(x, "linear_pred"), exp)
 })
 
 test_that("format_predictions() handles hazard type", {
   x <- c(0.1, 0.2, 0.3)
-  res <- format_predictions(x, "hazard")
+  exp <- tibble::tibble(.pred = x)
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred")
-  expect_equal(res$.pred, x)
+  expect_identical(format_predictions(x, "hazard"), exp)
 })
 
 test_that("format_predictions() handles raw type", {
   x <- list(a = 1, b = 2)
-  res <- format_predictions(x, "raw")
 
-  expect_equal(res, x)
+  expect_identical(format_predictions(x, "raw"), x)
 })
 
 test_that("format_predictions() handles conf_int type", {
   x <- data.frame(.pred_lower = c(1, 2), .pred_upper = c(3, 4))
-  res <- format_predictions(x, "conf_int")
+  exp <- tibble::tibble(.pred_lower = c(1, 2), .pred_upper = c(3, 4))
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, c(".pred_lower", ".pred_upper"))
-  expect_equal(res$.pred_lower, c(1, 2))
-  expect_equal(res$.pred_upper, c(3, 4))
+  expect_identical(format_predictions(x, "conf_int"), exp)
 })
 
 test_that("format_predictions() handles pred_int type", {
   x <- data.frame(.pred_lower = c(0, 1), .pred_upper = c(2, 3))
-  res <- format_predictions(x, "pred_int")
+  exp <- tibble::tibble(.pred_lower = c(0, 1), .pred_upper = c(2, 3))
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, c(".pred_lower", ".pred_upper"))
-  expect_equal(res$.pred_lower, c(0, 1))
-  expect_equal(res$.pred_upper, c(2, 3))
+  expect_identical(format_predictions(x, "pred_int"), exp)
 })
 
 test_that("format_predictions() handles quantile type", {
@@ -96,18 +74,15 @@ test_that("format_predictions() handles quantile type", {
       c(0.25, 0.5, 0.75)
     )
   )
-  res <- format_predictions(x, "quantile")
 
-  expect_s3_class(res, "tbl_df")
-  expect_named(res, ".pred_quantile")
-  expect_s3_class(res$.pred_quantile, "quantile_pred")
+  expect_identical(format_predictions(x, "quantile"), x)
 })
 
 test_that("format_predictions() returns unknown types unchanged", {
   x <- data.frame(a = 1:3)
   res <- format_predictions(x, "unknown_type")
 
-  expect_equal(res, x)
+  expect_identical(res, x)
 })
 
 test_that("format_predictions() matches individual format functions", {
@@ -115,35 +90,35 @@ test_that("format_predictions() matches individual format functions", {
 
   x_vec <- c(1.5, 2.5, 3.5)
 
-  expect_equal(
+  expect_identical(
     format_predictions(x_vec, "numeric"),
     format_num(x_vec)
   )
-  expect_equal(
+  expect_identical(
     format_predictions(x_vec, "time"),
     format_time(x_vec)
   )
-  expect_equal(
+  expect_identical(
     format_predictions(x_vec, "survival"),
     format_survival(x_vec)
   )
-  expect_equal(
+  expect_identical(
     format_predictions(x_vec, "linear_pred"),
     format_linear_pred(x_vec)
   )
-  expect_equal(
+  expect_identical(
     format_predictions(x_vec, "hazard"),
     format_hazard(x_vec)
   )
 
   x_factor <- factor(c("a", "b", "a"))
-  expect_equal(
+  expect_identical(
     format_predictions(x_factor, "class"),
     format_class(x_factor)
   )
 
   x_df <- data.frame(a = c(0.2, 0.3), b = c(0.8, 0.7))
-  expect_equal(
+  expect_identical(
     format_predictions(x_df, "prob"),
     format_classprobs(x_df)
   )
