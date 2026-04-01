40d10a5ade8e38c18fa2f13744e86c9809afbc80
Hannah Frick <hannah@posit.co>
2026-04-01 13:53:17 +0100

    add tests for `find_tune_id()`

--- tests/testthat/test-tune_args.R
+++ tests/testthat/test-tune_args.R
@@ -12,3 +12,21 @@ test_that("tune_id() returns NA for non-tune input", {
   expect_identical(tune_id("a"), NA_character_)
   expect_identical(tune_id(rlang::quos(NULL)), NA_character_)
 })
+
+# find_tune_id() ----------------------------------------------------------
+
+test_that("find_tune_id() finds tune() nested in expressions", {
+  expect_identical(find_tune_id(quote(log(tune()))), "")
+  expect_identical(find_tune_id(quote(log(tune("nested")))), "nested")
+})
+
+test_that("find_tune_id() returns NA for empty input", {
+  expect_identical(find_tune_id(list()), NA_character_)
+})
+
+test_that("find_tune_id() errors with multiple tune() in one arg", {
+  expect_snapshot(
+    error = TRUE,
+    find_tune_id(quote(c(tune(), tune())))
+  )
+})
