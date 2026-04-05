e43650a39c55ff5af68c094831f10fa625acb8e7
Max Kuhn <mxkuhn@gmail.com>
2026-04-05 13:10:22 -0400

    doc update

--- R/aaa_archive.R
+++ R/aaa_archive.R
@@ -91,6 +91,7 @@ model_info_table <-
                "ordinal_reg",      "classification",       "ordinalNet",       "ordered",
                "ordinal_reg",      "classification",             "vglm",       "ordered",
                "rand_forest",      "classification",    "ordinalForest",       "ordered",
+                   "tab_pfn",       "classification",          "tabpfn",              NA,
                 "linear_reg", "quantile regression",         "quantreg",              NA,
                "rand_forest", "quantile regression",              "grf",              NA,
                    "auto_ml",          "regression",              "h2o",          "agua",
@@ -156,5 +157,6 @@ model_info_table <-
                 "svm_linear",          "regression",          "kernlab",              NA,
                   "svm_poly",          "regression",          "kernlab",              NA,
                    "svm_rbf",          "regression",          "kernlab",              NA,
-                   "svm_rbf",          "regression",        "liquidSVM",              NA
+                   "svm_rbf",          "regression",        "liquidSVM",              NA,
+                   "tab_pfn",          "regression",           "tabpfn",              NA,
       )
