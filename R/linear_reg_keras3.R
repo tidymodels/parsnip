65e6138a9682cf66e72eb9db23547b63f446ad8a
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-04-21 10:04:40 -0700

    add keras3 roxygen stubs

---
+++ R/linear_reg_keras3.R
@@ -0,0 +1,11 @@
+#' Linear regression via keras3
+#'
+#' This model uses regularized least squares to fit models with numeric outcomes.
+#'
+#' @includeRmd man/rmd/linear_reg_keras3.md details
+#'
+#' @name details_linear_reg_keras3
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
---
+++ R/logistic_reg_keras3.R
@@ -0,0 +1,13 @@
+#' Logistic regression via keras3
+#'
+#' [keras3_mlp()] fits a generalized linear model for binary outcomes. A
+#' linear combination of the predictors is used to model the log odds of an
+#' event.
+#'
+#' @includeRmd man/rmd/logistic_reg_keras3.md details
+#'
+#' @name details_logistic_reg_keras3
+#' @keywords internal
+NULL
+
+# See inst/README-DOCS.md for a description of how these files are processed
